10f2c76cabbf98dfa0bb816bd096f57e8de9e040
1hachem <hb@big-mama.io>
2026-05-28 19:46:52 +0100

    chore: update site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 export const SITE_TITLE = "Hachem's blog";
-export const SITE_DESCRIPTION = "writing about things";
+export const SITE_DESCRIPTION = `Welcome to my personal blog, my name is Hachem, 
+I like to write about tech, relegion, startups, human relations, and lifestyle.`;
 export const SITE_AUTHOR = "Hachem Betrouni";
 export const POSTS_PER_PAGE = 10;
