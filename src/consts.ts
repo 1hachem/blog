fb74bbd5be29381d64bf4eb649526a0b3407cc1c
1hachem <hb@big-mama.io>
2026-07-27 21:07:47 +0100

    chore(site): rebrand to Dashiverse and tighten description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-export const SITE_TITLE = "Hachem's blog";
-export const SITE_DESCRIPTION = `Welcome to my personal blog, my name is Hachem, 
-I like to write about tech, relegion, startups, human relations, and lifestyle.`;
+export const SITE_TITLE = 'Dashiverse';
+export const SITE_DESCRIPTION = `Hachem's personal blog and portfolio`;
 export const SITE_AUTHOR = 'Hachem Betrouni';
 export const POSTS_PER_PAGE = 10;
